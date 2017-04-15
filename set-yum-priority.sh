6669394865d63d1a135826a25d5bbf8988e43406
root <root@localhost.localdomain>
2017-04-15 22:41:47 +0900

    yum dnf切り替え対応
    設定済み優先度の削除処理追加

--- set-yum-priority.sh
+++ set-yum-priority.sh
@@ -2,26 +2,91 @@
 
 #from https://support.sysally.net/projects/kb/wiki/Script_to_set_yum_priority#Script-to-set-yum-priority
 
-yum -y install yum-priorities
-grep -rl "priority=" /etc/yum.repos.d/ && ( echo -e "Found existing priority setting..exiting!" ;exit 1 )
+
+
+DNF=0
+
+rpm -q dnf
+
+DNF=`echo $?`
+
+MANAGE_COMMAND=""
+
+if [ ${DNF} -eq 0 ]; then
+   echo "dnf found. Use dnf."
+   MANAGE_COMMAND="dnf"
+else
+   echo "dnf not found. Search yum."
+    YUM=0
+    rpm -q yum
+    YUM=`echo $?`
+    if [ ${YUM} -eq 0 ]; then
+       echo "yum found. Use yum."
+       MANAGE_COMMAND="yum"
+       echo "Install yum-priorities."
+       PRIORITIES_INSTALL_COMMAND="${MANAGE_COMMAND} -y install yum-priorities"
+       `${PRIORITIES_INSTALL_COMMAND}` 
+    else
+       echo "yum not found. Error."
+       exit 1
+    fi
+fi
+
+REPOLIST_COMMAND="${MANAGE_COMMAND} repolist"
+
+THIS_DIR=$(cd $(dirname $0);pwd)
+TEMPDIR="${THIS_DIR}/tmp"
+TEMPFILE_1="${TEMPDIR}/repolist1.tmp"
+TEMPFILE_2="${TEMPDIR}/repolist2.tmp"
+TEMPFILE_3="${TEMPDIR}/repolist3.tmp"
+TEMPFILE_4="${TEMPDIR}/repolist4.tmp"
+rm -rf ${TEMPDIR}
+mkdir ${TEMPDIR}
+
 echo -e "Please use priority in steps of 5" 
 echo -e " eg: base extras and updates can have priority 1 , epel can have priority 5 etc" 
 echo -e " This helps to incorporate a repo with priority in between if required in future!" 
 echo -n "" 
-yum repolist|sed -n '/repo id/,/repolist:/p'|grep -v "repo id"|grep -v "repolist:"|awk '{print $1}' > repolist.tmp
+
+`${REPOLIST_COMMAND} > ${TEMPFILE_1}`
+cat ${TEMPFILE_1}|sed -e "s/\*//"> ${TEMPFILE_2}
+cat ${TEMPFILE_2}|sed -e "1,2d"> ${TEMPFILE_3}
+cat ${TEMPFILE_3}|awk '{print $1}' > ${TEMPFILE_4}
+
+
 echo "Enabled yum repos:" 
 echo "=======================" 
-cat repolist.tmp
+cat ${TEMPFILE_4}
 echo "=======================" 
 echo
-echo
-for repo in `cat repolist.tmp`
+
+
+REPODIR="/etc/yum.repos.d"
+grep -rl "priority=" /etc/yum.repos.d
+EXIST=`echo $?`
+if [ ${EXIST} -eq 0 ]; then
+   echo -e "Found existing priority setting..exiting! Delete it."
+   sed -n -e "/^\[/h; /priority *=/{ G; s/\n/ /; s/ity=/ity = /; p }" ${REPODIR}/*.repo | sort -k3n
+   for repo in `cat ${TEMPFILE_4}`
+   do
+           for file in `find ${REPODIR} -type f`
+           do
+                   sed -i "/^priority=.*$/d" $file
+           done
+   done
+fi
+
+
+for repo in `cat ${TEMPFILE_4}`
 do
         echo "priority for repo $repo = ?" 
         read priority
-        for file in `find /etc/yum.repos.d -type f`
+        for file in `find ${REPODIR} -type f`
         do
                 sed -i "/\[$repo\]/a\priority=$priority" $file
         done
 done
-rm -f repolist.tmp
+
+sed -n -e "/^\[/h; /priority *=/{ G; s/\n/ /; s/ity=/ity = /; p }" ${REPODIR}/*.repo | sort -k3n
+
+rm -rf ${TEMPDIR}
