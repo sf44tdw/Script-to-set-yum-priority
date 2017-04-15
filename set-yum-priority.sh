5077da4f90041011f34229e1c6a22a717c2739b2
root <root@localhost.localdomain>
2017-04-15 23:48:01 +0900

    処理見直し

--- set-yum-priority.sh
+++ set-yum-priority.sh
@@ -2,24 +2,26 @@
 
 #from https://support.sysally.net/projects/kb/wiki/Script_to_set_yum_priority#Script-to-set-yum-priority
 
-
-
-DNF=0
-
-rpm -q dnf
-
-DNF=`echo $?`
+REPODIR="/etc/yum.repos.d"
+get_priority_info(){
+  sed -n -e "/^\[/h; /priority *=/{ G; s/\n/ /; s/ity=/ity = /; p }" ${REPODIR}/*.repo | sort -k3n
+}
 
 MANAGE_COMMAND=""
+
+DNF=0
+rpm -q dnf
+DNF=`echo $?`
+
+YUM=0
+rpm -q yum
+YUM=`echo $?`
 
 if [ ${DNF} -eq 0 ]; then
    echo "dnf found. Use dnf."
    MANAGE_COMMAND="dnf"
 else
    echo "dnf not found. Search yum."
-    YUM=0
-    rpm -q yum
-    YUM=`echo $?`
     if [ ${YUM} -eq 0 ]; then
        echo "yum found. Use yum."
        MANAGE_COMMAND="yum"
@@ -53,20 +55,17 @@ cat ${TEMPFILE_1}|sed -e "s/\*//"> ${TEMPFILE_2}
 cat ${TEMPFILE_2}|sed -e "1,2d"> ${TEMPFILE_3}
 cat ${TEMPFILE_3}|awk '{print $1}' > ${TEMPFILE_4}
 
-
 echo "Enabled yum repos:" 
 echo "=======================" 
 cat ${TEMPFILE_4}
 echo "=======================" 
 echo
 
-
-REPODIR="/etc/yum.repos.d"
-grep -rl "priority=" /etc/yum.repos.d
+grep -rl "priority=" ${REPODIR}
 EXIST=`echo $?`
 if [ ${EXIST} -eq 0 ]; then
    echo -e "Found existing priority setting..exiting! Delete it."
-   sed -n -e "/^\[/h; /priority *=/{ G; s/\n/ /; s/ity=/ity = /; p }" ${REPODIR}/*.repo | sort -k3n
+   get_priority_info
    for repo in `cat ${TEMPFILE_4}`
    do
            for file in `find ${REPODIR} -type f`
@@ -75,7 +74,6 @@ if [ ${EXIST} -eq 0 ]; then
            done
    done
 fi
-
 
 for repo in `cat ${TEMPFILE_4}`
 do
@@ -87,6 +85,6 @@ do
         done
 done
 
-sed -n -e "/^\[/h; /priority *=/{ G; s/\n/ /; s/ity=/ity = /; p }" ${REPODIR}/*.repo | sort -k3n
+get_priority_info
 
 rm -rf ${TEMPDIR}
