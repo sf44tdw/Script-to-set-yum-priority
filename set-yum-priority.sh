db540fe650795e534d58d6a76a6fca2a51d8b9b7
root <root@localhost.localdomain>
2019-07-04 23:19:24 +0900

    不要箇所修正

--- set-yum-priority.sh
+++ set-yum-priority.sh
@@ -41,7 +41,7 @@ shift $((OPTIND - 1))
 #多重起動防止機講
 # 同じ名前のプロセスが起動していたら起動しない。
 _lockfile="/tmp/`basename $0`.lock"
-ln -s /dummy $_lockfile 2> /dev/null || { echo 'Cannot run multiple instance.'  >>${LOGFILE}; exit 9; }
+ln -s /dummy $_lockfile 2> /dev/null || { echo 'Cannot run multiple instance.'; exit 9; }
 trap "rm "${_lockfile}"; exit" 1 2 3 15
 
 MANAGE_COMMAND=""
